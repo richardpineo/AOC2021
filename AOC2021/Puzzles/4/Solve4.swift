873a029ebb0b887b14180201a6029ea416dde359
Richard Pineo <Richard.E.Pineo@ge.com>
2021-12-04 21:53:47 -0800

    formatting

--- AOC2021/Puzzles/4/Solve4.swift
+++ AOC2021/Puzzles/4/Solve4.swift
@@ -13,33 +13,34 @@ class Solve4: PuzzleSolver {
 
 	var answerA = "25410"
 	var answerB = "2730"
-	
+
 	class Board {
 		struct Spot {
 			var num: Int
 			var marked: Bool
 		}
+
 		var spots: [Spot]
-		
+
 		init(spotStrs: [String]) {
 			spots = []
-			for row in 0..<spotStrs.count {
+			for row in 0 ..< spotStrs.count {
 				let nums = spotStrs[row].components(separatedBy: " ").filter { !$0.isEmpty }
 				let cols = nums.map { Spot(num: Int($0)!, marked: false) }
 				spots.append(contentsOf: cols)
 			}
 		}
-		
+
 		func mark(num: Int) {
-			guard let spotIndex = spots.firstIndex( where: { $0.num == num } ) else {
+			guard let spotIndex = spots.firstIndex(where: { $0.num == num }) else {
 				return
 			}
 			spots[spotIndex] = .init(num: num, marked: true)
 		}
 
 		var bingo: Bool {
-			for idx1 in 0...4 {
-				let idx2 = 0...4
+			for idx1 in 0 ... 4 {
+				let idx2 = 0 ... 4
 				if idx2.allSatisfy({ spots[Position2D(idx1, $0).arrayIndex(numCols: 5)].marked }) {
 					return true
 				}
@@ -49,9 +50,9 @@ class Solve4: PuzzleSolver {
 			}
 			return false
 		}
-		
+
 		var sumUnmarked: Int {
-			spots.reduce(0 ) { $0 + ($1.marked ? 0 : $1.num) }
+			spots.reduce(0) { $0 + ($1.marked ? 0 : $1.num) }
 		}
 	}
 
@@ -62,7 +63,7 @@ class Solve4: PuzzleSolver {
 	func solveB() -> String {
 		solveB("Input4").description
 	}
-	
+
 	struct Game {
 		let pulls: [Int]
 		let boards: [Board]
@@ -70,31 +71,31 @@ class Solve4: PuzzleSolver {
 
 	private func loadGame(_ fileName: String) -> Game {
 		let input = FileHelper.load(fileName)!.filter { !$0.isEmpty }
-		
+
 		let pulls = input[0].description.components(separatedBy: ",").map { Int($0)! }
-		
+
 		var boards: [Board] = []
 		var lineNum = 1
 		while lineNum < input.count {
 			var spotStrs: [String] = []
-			for boardLine in 0...4 {
-				spotStrs.append(input[lineNum+boardLine])
+			for boardLine in 0 ... 4 {
+				spotStrs.append(input[lineNum + boardLine])
 			}
 			let board = Board(spotStrs: spotStrs)
 			boards.append(board)
-			
+
 			lineNum += 5
 		}
 		return .init(pulls: pulls, boards: boards)
 	}
-	
+
 	func solveA(_ fileName: String) -> Int {
 		let game = loadGame(fileName)
 
-		for pullIndex in 0..<game.pulls.count {
+		for pullIndex in 0 ..< game.pulls.count {
 			let pull = game.pulls[pullIndex]
-			game.boards.forEach { $0.mark(num: pull)}
-			
+			game.boards.forEach { $0.mark(num: pull) }
+
 			if let bingo = game.boards.first(where: { $0.bingo }) {
 				return bingo.sumUnmarked * pull
 			}
@@ -104,17 +105,17 @@ class Solve4: PuzzleSolver {
 
 	func solveB(_ fileName: String) -> Int {
 		let game = loadGame(fileName)
-		
-		for pullIndex in 0..<game.pulls.count {
+
+		for pullIndex in 0 ..< game.pulls.count {
 			let pull = game.pulls[pullIndex]
-			
+
 			guard let bingoLess = game.boards.first(where: { !$0.bingo }) else {
 				return -666
 			}
-			
-			game.boards.forEach { $0.mark(num: pull)}
-			
-			if game.boards.allSatisfy( { $0.bingo } ) {
+
+			game.boards.forEach { $0.mark(num: pull) }
+
+			if game.boards.allSatisfy(\.bingo) {
 				return bingoLess.sumUnmarked * pull
 			}
 		}
--- AOC2021/Puzzles/5/Solve5.swift
+++ AOC2021/Puzzles/5/Solve5.swift
@@ -21,15 +21,15 @@ class Solve5: PuzzleSolver {
 	func solveB() -> String {
 		solve("Input5", orthogonalOnly: false).description
 	}
-	
+
 	struct Line {
 		var p1: Position2D
 		var p2: Position2D
 	}
-	
+
 	func solve(_ fileName: String, orthogonalOnly: Bool) -> Int {
 		let input = FileHelper.load(fileName)!.filter { !$0.isEmpty }
-		
+
 		let lines: [Line] = input.map {
 			let tokens = $0.components(separatedBy: [" ", ","])
 			return Line(
@@ -37,44 +37,43 @@ class Solve5: PuzzleSolver {
 				p2: .init(Int(tokens[3])!, Int(tokens[4])!)
 			)
 		}
-		
+
 		let filteredLines = orthogonalOnly ? lines.filter { $0.p1.x == $0.p2.x || $0.p1.y == $0.p2.y } : lines
-		
-		var coverage: Dictionary<Position2D, Int> = [:]
+
+		var coverage: [Position2D: Int] = [:]
 
 		func add(_ p: Position2D) {
 			let c = coverage[p] ?? 0
 			coverage[p] = c + 1
 		}
-		
+
 		filteredLines.forEach {
-		
 			let rangeX = $0.p2.x - $0.p1.x
 			let rangeY = $0.p2.y - $0.p1.y
 			let numSteps = max(abs(rangeX), abs(rangeY))
 			let stepX = rangeX / numSteps
 			let stepY = rangeY / numSteps
-			
-			for step in 0...numSteps {
+
+			for step in 0 ... numSteps {
 				add(.init($0.p1.x + step * stepX, $0.p1.y + step * stepY))
 			}
 		}
-		
+
 		let count = coverage.reduce(0) {
 			$0 + ($1.value > 1 ? 1 : 0)
 		}
 		return count
-		
+
 		/* Debug
-		func char(_ p: Position2D) -> String {
-			if let v = coverage[p] {
-				return v.description
-			}
-			return "."
-		}
-		
-		for col in 0...9 {
-			print((0...9).map( { char(.init($0, col)) }).joined())
-		} */
+		 func char(_ p: Position2D) -> String {
+		 	if let v = coverage[p] {
+		 		return v.description
+		 	}
+		 	return "."
+		 }
+
+		 for col in 0...9 {
+		 	print((0...9).map( { char(.init($0, col)) }).joined())
+		 } */
 	}
 }
