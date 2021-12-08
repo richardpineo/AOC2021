4713dbad80d80f9db52aaab0c1cdf70bd8d92cca
Richard Pineo <Richard.E.Pineo@ge.com>
2021-12-07 20:30:04 -0800

    7b

--- AOC2021/Puzzles/7/Solve7.swift
+++ AOC2021/Puzzles/7/Solve7.swift
@@ -8,22 +8,30 @@ class Solve7: PuzzleSolver {
 	}
 
 	func solveBExamples() -> Bool {
-		true
+		solve("Example7", fuelCalc: fuelCostB) == 168
 	}
 
 	var answerA = "342641"
-	var answerB = ""
+	var answerB = "93006301"
 
 	func solveA() -> String {
 		solve("Input7", fuelCalc: fuelCostA).description
 	}
 
 	func solveB() -> String {
-		""
+		solve("Input7", fuelCalc: fuelCostB).description
 	}
-	
+
 	func fuelCostA(crabs: [Int], offset: Int) -> Int {
 		crabs.reduce(0) { $0 + abs($1 - offset) }
+	}
+
+	func fuelCostB(crabs: [Int], offset: Int) -> Int {
+		crabs.reduce(0) {
+			let distance = abs($1 - offset)
+			let fuel = distance * (distance + 1) / 2
+			return $0 + fuel
+		}
 	}
 
 	func solve(_ fileName: String, fuelCalc: ([Int], Int) -> Int) -> Int {
