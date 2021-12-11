c279ecdd86fa8d68369cbfe5f2e627421d93c3b5
Richard Pineo <Richard.E.Pineo@ge.com>
2021-12-10 18:44:46 -0800

    10b

--- AOC2021/Puzzles/10/Solve10.swift
+++ AOC2021/Puzzles/10/Solve10.swift
@@ -8,41 +8,48 @@ class Solve10: PuzzleSolver {
 	}
 
 	func solveBExamples() -> Bool {
-		true
+		solveB("Example10") == 288_957
 	}
 
-	var answerA = ""
-	var answerB = ""
+	var answerA = "319329"
+	var answerB = "3515583998"
 
 	func solveA() -> String {
 		solveA("Input10").description
 	}
 
 	func solveB() -> String {
-		""
+		solveB("Input10").description
 	}
-	
-	private let pairs: Dictionary<Character, Character> = [
-		")" : "(",
-		"]" : "[",
-		"}" : "{",
-		">" : "<",
+
+	private let pairs: [Character: Character] = [
+		")": "(",
+		"]": "[",
+		"}": "{",
+		">": "<",
 	]
-	
-	private let scores: Dictionary<Character, Int> = [
-		")" : 3,
-		"]" : 57,
-		"}" : 1197,
-		">" : 25137,
+
+	private let invalidScores: [Character: Int] = [
+		")": 3,
+		"]": 57,
+		"}": 1197,
+		">": 25137,
 	]
-	
+
+	private let completionScores: [Character: Int] = [
+		")": 1,
+		"]": 2,
+		"}": 3,
+		">": 4,
+	]
+
 	func findInvalid(_ s: String) -> Character? {
 		var current = Stack<Character>()
 		for c in s {
 			switch c {
 			case "(", "[", "{", "<":
 				current.push(c)
-								
+
 			default:
 				let shouldBe = pairs[c]
 				if shouldBe != current.pop() {
@@ -53,16 +60,44 @@ class Solve10: PuzzleSolver {
 		return nil
 	}
 
+	func complete(_ s: String) -> String {
+		var current = Stack<Character>()
+		for c in s {
+			switch c {
+			case "(", "[", "{", "<":
+				current.push(c)
+
+			default:
+				_ = current.pop()
+			}
+		}
+		// what's left is our stuffs.
+		var answer = ""
+		while let c = current.pop() {
+			let toAdd = pairs.first { $0.value == c }!.key
+			answer.append(toAdd)
+		}
+		return answer
+	}
+
 	func solveA(_ fileName: String) -> Int {
 		let values = FileHelper.load(fileName)!.filter { !$0.isEmpty }
 		let invalid = values.compactMap { findInvalid($0) }
 		let score = invalid.reduce(0) {
-			$0 + scores[$1]!
+			$0 + invalidScores[$1]!
 		}
 		return score
 	}
 
 	func solveB(_ fileName: String) -> Int {
-		0
+		let values = FileHelper.load(fileName)!.filter { !$0.isEmpty }
+		let valid = values.compactMap { findInvalid($0) == nil ? $0 : nil }
+		let completions = valid.map { complete($0) }
+		let scores = completions.map {
+			$0.reduce(0) {
+				$0 * 5 + completionScores[$1]!
+			}
+		}.sorted()
+		return scores[scores.count / 2]
 	}
 }
