a68ff41df7fe2b57e54d2cfe72bd98f9cd05a218
Richard Pineo <Richard.E.Pineo@ge.com>
2021-12-11 15:24:09 -0800

    11a

--- AOC2021/Puzzles/11/Solve11.swift
+++ AOC2021/Puzzles/11/Solve11.swift
@@ -11,7 +11,7 @@ class Solve11: PuzzleSolver {
 		true
 	}
 
-	var answerA = ""
+	var answerA = "1608"
 	var answerB = ""
 
 	func solveA() -> String {
@@ -22,9 +22,71 @@ class Solve11: PuzzleSolver {
 		solveB("").description
 	}
 
+	func increment(_ grid: inout Grid2D) {
+		for x in 0 ..< grid.maxPos.x {
+			for y in 0 ..< grid.maxPos.y {
+				let pos = Position2D(x,y)
+				let val = grid.value(pos)
+				grid.setValue(pos, val + 1)
+			}
+		}
+	}
+	
+	func flash(_ grid: inout Grid2D) -> Int {
+		var flashCount = 0
+		for x in 0 ..< grid.maxPos.x {
+			for y in 0 ..< grid.maxPos.y {
+				let pos = Position2D(x,y)
+				let val = grid.value(pos)
+				if val > 9 {
+					flashCount += 1
+					// increment all the neighbors
+					let neighbors = grid.neighbors(pos, includePos: false, includeDiagonals: true)
+					neighbors.forEach {
+						if grid.value($0) != 0 {
+							grid.setValue($0, grid.value($0) + 1)
+						}
+					}
+					grid.setValue(pos, 0)
+				}
+			}
+		}
+
+		return flashCount
+	}
+
 	func solveA(_ fileName: String) -> Int {
-		let grid = Grid2D(fileName: fileName)
-		return 0
+		var grid = Grid2D(fileName: fileName)
+		
+		// Turn off to hide printing.
+		let debug = true
+		
+		print("Before any steps:\n\(grid.debugDisplay)")
+
+		var flashCount = 0
+		for i in 0..<100 {
+
+			if debug {
+			if i == 10 {
+				print("After 10, \(flashCount) flashes (expect 204)")
+			}
+			}
+			
+			increment(&grid)
+
+			var didFlash = true
+			while didFlash {
+				let thisCount = flash(&grid)
+				didFlash = thisCount > 0
+				flashCount += thisCount
+			}
+
+			if debug {
+			print("After step \(i+1):\n\(grid.debugDisplay)")
+			}
+		}
+		
+		return flashCount
 	}
 
 	func solveB(_ fileName: String) -> Int {
--- AOC2021Tests/AOC2021Tests.swift
+++ AOC2021Tests/AOC2021Tests.swift
@@ -5,7 +5,7 @@ import XCTest
 
 class Test2021: XCTestCase {
 	func testOne() throws {
-		testOne(Solve10())
+		testOne(Solve11())
 	}
 
 	func testAll() throws {
