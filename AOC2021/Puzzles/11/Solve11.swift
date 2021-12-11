89cf7dcd030f4e77233c686f2d09302b837a3c05
Richard Pineo <Richard.E.Pineo@ge.com>
2021-12-11 15:57:52 -0800

    so slow

--- AOC2021/Puzzles/11/Solve11.swift
+++ AOC2021/Puzzles/11/Solve11.swift
@@ -8,7 +8,7 @@ class Solve11: PuzzleSolver {
 	}
 
 	func solveBExamples() -> Bool {
-		true
+		solveB("Example11") == 195
 	}
 
 	var answerA = "1608"
@@ -19,15 +19,14 @@ class Solve11: PuzzleSolver {
 	}
 
 	func solveB() -> String {
-		solveB("").description
+		solveB("Input11").description
 	}
 
 	func increment(_ grid: inout Grid2D) {
 		for x in 0 ..< grid.maxPos.x {
 			for y in 0 ..< grid.maxPos.y {
 				let pos = Position2D(x,y)
-				let val = grid.value(pos)
-				grid.setValue(pos, val + 1)
+				grid.setValue(pos, grid.value(pos) + 1)
 			}
 		}
 	}
@@ -37,41 +36,28 @@ class Solve11: PuzzleSolver {
 		for x in 0 ..< grid.maxPos.x {
 			for y in 0 ..< grid.maxPos.y {
 				let pos = Position2D(x,y)
-				let val = grid.value(pos)
-				if val > 9 {
+				if grid.value(pos) > 9 {
 					flashCount += 1
-					// increment all the neighbors
+
 					let neighbors = grid.neighbors(pos, includePos: false, includeDiagonals: true)
 					neighbors.forEach {
 						if grid.value($0) != 0 {
 							grid.setValue($0, grid.value($0) + 1)
 						}
 					}
+					
 					grid.setValue(pos, 0)
 				}
 			}
 		}
-
 		return flashCount
 	}
 
 	func solveA(_ fileName: String) -> Int {
 		var grid = Grid2D(fileName: fileName)
-		
-		// Turn off to hide printing.
-		let debug = true
-		
-		print("Before any steps:\n\(grid.debugDisplay)")
 
 		var flashCount = 0
-		for i in 0..<100 {
-
-			if debug {
-			if i == 10 {
-				print("After 10, \(flashCount) flashes (expect 204)")
-			}
-			}
-			
+		for _ in 0..<100 {
 			increment(&grid)
 
 			var didFlash = true
@@ -80,16 +66,30 @@ class Solve11: PuzzleSolver {
 				didFlash = thisCount > 0
 				flashCount += thisCount
 			}
-
-			if debug {
-			print("After step \(i+1):\n\(grid.debugDisplay)")
-			}
 		}
 		
 		return flashCount
 	}
 
 	func solveB(_ fileName: String) -> Int {
-		0
+		var grid = Grid2D(fileName: fileName)
+		
+		let maxSteps = 9999999
+		let syncCount = grid.maxPos.x * grid.maxPos.y - 1
+		for i in 0..<maxSteps {
+			increment(&grid)
+
+			var didFlash = true
+			while didFlash {
+				let thisCount = flash(&grid)
+				if thisCount == syncCount {
+					// found it
+					return i
+				}
+				didFlash = thisCount > 0
+			}
+		}
+		
+		return -666
 	}
 }
