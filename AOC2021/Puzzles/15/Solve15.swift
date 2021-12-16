15e675e680aa11efbf95cf95c657a555545f69db
Richard Pineo <Richard.E.Pineo@ge.com>
2021-12-15 21:05:37 -0800

    format

--- AOC2021/Puzzles/15/Solve15.swift
+++ AOC2021/Puzzles/15/Solve15.swift
@@ -15,7 +15,7 @@ class Solve15: PuzzleSolver {
 	var answerB = "3022"
 
 	var shouldTestB: Bool = false
-	
+
 	func solveA() -> String {
 		solveA("Input15").description
 	}
@@ -23,14 +23,14 @@ class Solve15: PuzzleSolver {
 	func solveB() -> String {
 		solveB("Input15").description
 	}
-	
+
 	func walk(risks: Grid2D) -> Int {
-		let start = Position2D(0,0)
+		let start = Position2D(0, 0)
 		var lowestCosts = Grid2D(maxPos: risks.maxPos, initialValue: Int.max)
 		var toWalk = Queue<Position2D>()
 		toWalk.enqueue(start)
 		lowestCosts.setValue(start, 0)
-		
+
 		while let nextPos = toWalk.dequeue() {
 			risks.neighbors(nextPos, includePos: false, includeDiagonals: false).forEach {
 				let walkCost = lowestCosts.value(nextPos) + risks.value($0)
@@ -40,7 +40,7 @@ class Solve15: PuzzleSolver {
 				}
 			}
 		}
-		return lowestCosts.value(.init(risks.maxPos.x-1, risks.maxPos.y-1))
+		return lowestCosts.value(.init(risks.maxPos.x - 1, risks.maxPos.y - 1))
 	}
 
 	func solveA(_ fileName: String) -> Int {
@@ -51,12 +51,12 @@ class Solve15: PuzzleSolver {
 
 	func solveB(_ fileName: String) -> Int {
 		let risks = Grid2D(fileName: fileName)
-		var megaRisks = Grid2D(maxPos: .init(risks.maxPos.x*5, risks.maxPos.y*5), initialValue: -1)
-		for x in 0..<risks.maxPos.x {
-			for y in 0..<risks.maxPos.y {
-				for mx in 0..<5 {
-					for my in 0..<5 {
-						let val = risks.value(.init(x,y))
+		var megaRisks = Grid2D(maxPos: .init(risks.maxPos.x * 5, risks.maxPos.y * 5), initialValue: -1)
+		for x in 0 ..< risks.maxPos.x {
+			for y in 0 ..< risks.maxPos.y {
+				for mx in 0 ..< 5 {
+					for my in 0 ..< 5 {
+						let val = risks.value(.init(x, y))
 						var megaVal = val + mx + my
 						if megaVal > 9 {
 							megaVal -= 9
