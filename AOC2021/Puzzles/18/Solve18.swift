746e336bf05f8f162f2282e7d956bb44d94327d7
Richard Pineo <Richard.E.Pineo@ge.com>
2021-12-19 14:37:20 -0800

    now with parents

--- AOC2021/Puzzles/18/Solve18.swift
+++ AOC2021/Puzzles/18/Solve18.swift
@@ -5,14 +5,13 @@ import Foundation
 class Solve18: PuzzleSolver {
 	func solveAExamples() -> Bool {
 		let loaded = Self.loadTests.allSatisfy {
-			let tree = load($0)
-			print("\($0) -> \(tree.description)")
-			return tree.description == $0
+			let n = load($0)
+			print("\($0) -> \(n.description)")
+			return n.description == $0
 		}
 		if !loaded {
 			return false
 		}
-
 		/*
 		 explodeTests
 
@@ -44,67 +43,65 @@ class Solve18: PuzzleSolver {
 		""
 	}
 	
-	typealias SnailTree = BinaryTree<Int>
+	class Node {
+		init(contents: Element?, parent: Node?) {
+			self.contents = contents
+			self.parent = parent
+		}
+		var parent: Node?
+		var contents: Element?
+		
+		var description: String {
+			return contents?.description ?? ""
+		}
+	}
 
-
-	/*
 	indirect enum Element {
 		case number(Int)
-		case pair(Pair)
-
-		var debugDescription: String {
+		case pair(Node, Node)
+		
+		var description: String {
 			switch self {
 			case let .number(n):
 				return String(n)
-			case let .pair(p):
-				return p.description
+			case let .pair(n1, n2):
+				return  String("[\(n1.description),\(n2.description)]")
 			}
 		}
 	}
-	 */
 
-	func add(_ e1: SnailTree, _ e2: SnailTree) -> SnailTree {
-		.node(e1, 0, e2)
-	}
-
-	func explode(e _: SnailTree) -> Bool{
-		// Find the first one at depth 4
-		return false
-	}
-/*
-	struct Pair {
-		var first: Element
-		var second: Element
-
-		var description: String {
-			String("[\(first.debugDescription),\(second.debugDescription)]")
-		}
-	}
- */
-
-	func load(_ s: String, pos: inout Int) -> SnailTree {
+	func load(_ s: String, parent: Node?, pos: inout Int) -> Node {
 		switch s.character(at: pos) {
 		case "[":
+			let child = Node(contents: nil, parent: parent)
 			pos += 1
-			let first = load(s, pos: &pos)
+			let first = load(s, parent: child, pos: &pos)
 			pos += 1
-			let second = load(s, pos: &pos)
+			let second = load(s, parent: child, pos: &pos)
 			pos += 1
-			return .node(first, 0, second)
-
+			child.contents = .pair(first, second)
+			return child
+			
 		case "0" ... "9":
 			let val = Int(String(s.character(at: pos)))!
 			pos += 1
-			return .node(.empty, val, .empty)
+			return .init(contents: .number(val), parent: parent)
 
 		default:
 			break
 		}
-		return .empty
+		return .init(contents: .number(-666), parent: parent)
 	}
 
-	func load(_ s: String) -> SnailTree {
+	func load(_ s: String) -> Node {
 		var pos = 0
-		return load(s, pos: &pos)
+		return load(s, parent: nil, pos: &pos)
+	}
+	
+	func add(_ e1: inout Node, _ e2: inout Node) -> Node {
+		let node = Node(contents: .pair(e1, e2), parent: nil)
+		e1.parent = node
+		e2.parent = node
+		return node
 	}
 }
